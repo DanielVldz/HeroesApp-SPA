2767fb33df6a51c943725c059d021fd257f537e6
DanielVz <danielrvp27@gmail.com>
2023-04-04 23:57:47 -0700

    Proyecto base e inicio de rutas

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navbar } from '../ui';
+import { LoginPage } from '../auth';
+import { MarvelPage, DcPage } from '../heroes';
+
+
+
+export const AppRouter = () => {
+  return (
+    <>
+    <Navbar/>
+        <Routes>
+            <Route path="marvel" element={<MarvelPage/>} />
+            <Route path="dc" element={<DcPage/>} />
+            <Route path="login" element={<LoginPage/>} />
+            <Route path="/" element={<Navigate to="/marvel"/>} />
+        </Routes>
+    </>
+  )
+}
